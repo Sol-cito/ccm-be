37b11017a2ee7b2e505a0b49430ddd90a86e0d29
dataenggu <dsolkim1030@gmail.com>
2021-11-01 20:24:47 +0900

    create python crawler for country codes

--- src/python/countryCodeCrawler.py
+++ src/python/countryCodeCrawler.py
@@ -1,20 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+def handleSpecialCharacter(input):
+    output = ""
+    for i in range(len(input)):
+        if input[i] == "'": continue
+        output += input[i]
+    return output
+
+
 print("----- Crawling Start..-----")
 URL = 'https://www.iban.com/country-codes'
+flyway_fileName = 'V1_101__insert_into_country_code_table.sql'
 
 htmlResponse = requests.get(URL).text
+
+newFile = open(flyway_fileName, 'w', encoding='UTF-8')
 
 countryCodeRows = BeautifulSoup(htmlResponse, 'html.parser').find_all('tr')
 for i in range(1, len(countryCodeRows)):
     eachItem = countryCodeRows[i].find_all('td')
-    print()
-    print("INSERT INTO country_code VALUES (",
-          "country_name = '", eachItem[0].text + "', ",
-          "alpha2_code = '", eachItem[1].text + "', ",
-          "alpha3_code = '", eachItem[2].text + "', ",
-          "numeric_code = ", eachItem[3].text,
-          " );", sep="")
+    row = "INSERT INTO country_code (country_name, alpha2_code, alpha3_code, numeric_code) VALUES (" \
+          + "'" + handleSpecialCharacter(eachItem[0].text) \
+          + "', " \
+          + "'" + eachItem[1].text \
+          + "', " \
+          + "'" + eachItem[2].text \
+          + "', " \
+          + "" + eachItem[3].text \
+          + " );"
+    newFile.write(row)
 
 print("----- END -----")
